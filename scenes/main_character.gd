5034e40e18c73e9c0b98218bf8f73d516385301f
Dimitri Geers <31861391+DimitriGeers@users.noreply.github.com>
2024-06-27 22:56:18 +0200

    added giant retard sign
    For hunnybunny

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -22,18 +22,24 @@ func _physics_process(delta):
 		sprite_2d.animation = "jumping"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
+	
+	if (direction == -1):
+		sprite_2d.flip_h = true
+	elif (direction == 1):
+		sprite_2d.flip_h = false
+		
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
 
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
+	
